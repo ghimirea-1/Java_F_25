82f4c499ece1bb126a4dc51c1db19515ffd34d9c
ghimirea-1 <aishghimi@gmail.com>
2025-03-31 09:57:39 -0500

    Update DbQuiz.java

--- DbQuiz.java
+++ DbQuiz.java
@@ -1,11 +1,13 @@
 package Db_Quiz;
 import java.io.*;
+import java.util.Scanner;
 import java.sql.*;
 	
 
 public class DbQuiz {
 
 	public static void main(String[] args) throws Exception {
+		Scanner scnr = new Scanner(System.in);
 		String url = "jdbc:mysql://localhost:3306/quiz_db"; // DB details
 		String username = "root"; // MySQL credentials
 		String password = "";
@@ -21,14 +23,79 @@ public class DbQuiz {
 		String user_name = rs.getString("Username");
 		String pass_word = rs.getString("Password");
 		String email = rs.getString("Email");
+		System.out.println("Input your data: ");
 		
-		System.out.println(full_name); // Print result on console
-		System.out.println(user_name);
-		System.out.println(pass_word);
-		System.out.println(email);
+		System.out.println("Name: ");
+		String full_name1 = scnr.nextLine();
+		System.out.println("User name: ");
+		String user_name1 = scnr.nextLine();
+		System.out.println("Password: ");
+		String pass_word1 = scnr.nextLine();
+		System.out.println("Email: ");
+		String email1 = scnr.nextLine();
+		
+
+		
+		if (full_name.equalsIgnoreCase(full_name1)) {
+			System.out.println("Name found");
+			if (user_name.equalsIgnoreCase(user_name1)) {
+				System.out.println("Username found");
+				if (pass_word.equalsIgnoreCase(pass_word1)) {
+					System.out.println("Password found");
+					if (email.equalsIgnoreCase(email1)) {
+						System.out.println("Email found");
+						System.out.println("The information matches!");
+				}
+			}
+		}
+		}
+		
+	
+		System.out.println("Welcome to Java Quest BUDDY!");
+		System.out.println("Level: ");
+		String level1 = scnr.nextLine();
+		System.out.println(level1 + " Level Selected!");
+			
+	
+		
 		st.close(); // close statement
 		con.close(); // close connection
 		System.out.println("Connection Closed....");
 	}
-
 }
+
+       /* int easyScore = 0, mediumScore = 0, hardScore = 0;
+        
+        
+        easyScore = runQuiz(scnr, "EASY", easyQuestions, easyChoices, easyAnswers, explanations, 0);
+        mediumScore = runQuiz(scnr, "MEDIUM", mediumQuestions, mediumChoices, mediumAnswers, explanations, easyQuestions.length);
+        hardScore = runQuiz(scnr, "HARD", hardQuestions, hardChoices, hardAnswers, explanations, easyQuestions.length + mediumQuestions.length);
+
+		
+        
+		
+
+	    public static int runQuiz(Scanner scnr, String level, String[] questions, String[][] choices, String[] answers, String[] explanations, int offset) {
+	        int score = 0;
+	        System.out.println("\n" + level + " Questions:");
+	        for (int i = 0; i < questions.length; i++) {
+	            System.out.println((i + 1) + ". " + questions[i]);
+	            for (String option : choices[i]) {
+	                System.out.println(option);
+	            }
+	            System.out.print("Your answer: ");
+	            String userAnswer = scnr.nextLine();
+	            if (userAnswer.equalsIgnoreCase(answers[i])) {
+	                System.out.println("Correct!\n");
+	                score++;
+	            } else {
+	                System.out.println("Incorrect! The correct answer is: " + answers[i]);
+	                System.out.println("Explanation: " + explanations[offset + i] + "\n");
+	            }
+	        }
+	        return score;
+	   
+        	*/
+
+
+
